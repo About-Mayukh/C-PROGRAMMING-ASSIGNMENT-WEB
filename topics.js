4ca5741b63c07476ca2338b84bae8c4d606d6ce7
MAYUKH DEY <mayukhdey9920.apple@gmail.com>
2026-07-18 00:29:15 +0530

    Expand topics array with additional programming tasks
    id added till 16

--- topics.js
+++ topics.js
@@ -30,6 +30,56 @@ const topics = [
     },
 
     {
+        id: 7,
+        title: 'Write a program to accept the length and breadth of a rectangle and calculate its area.'
+    },
+
+    {
+        id: 8,
+        title: 'Write a program to convert temperature from Celsius to Fahrenheit.'
+    },
+
+    {
+        id: 9,
+        title: 'Write a program to convert temperature from Fahrenheit to Celsius.'
+    },
+
+    {
+        id: 10,
+        title: 'Write a program to accept marks of five subjects and calculate the total marks and percentage.'
+    },
+
+    {
+        id: 11,
+        title: 'Write a program to accept two numbers and determine which number is greater (Without using if-else).'
+    },
+
+    {
+        id: 12,
+        title: 'Write a program to acceptthree numbers and determine which number is greater (Without using if-else).'
+    },
+
+    {
+        id: 13,
+        title: 'Write a program to accept a number and check whether it is positive or negative or zero (Without using if-else).'
+    },
+
+    {
+        id: 14,
+        title: ' Write a program to swap two numbers using a third variable.'
+    },
+
+    {
+        id: 15,
+        title: 'Write a program to swap two numbers without using a third variable.'
+    },
+
+    {
+        id: 16,
+        title: 'Write a program to accept two numbers and check whether they are equal or not.'
+    },
+
+    {
         id: 524,
         title: 'Login Credentials System'
     },
